1c5c79b4fdbace098eea27fb2d93a84d11878f58
Edson Nascimento <edsnascimento89@gmail.com>
2023-02-06 11:01:36 -0300

    [new] : building interface mobile

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -13,7 +13,10 @@ const weekDays = [
 
 const summaryDates = generateDatesFromYearBeginning()
 
-console.log(summaryDates); 
+const minimumSummaryDatesSize = 18 * 7 //18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
+// console.log(summaryDates); 
 
 export function SummaryTable() {
     return (
@@ -32,22 +35,14 @@ export function SummaryTable() {
             </div>
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
+              {summaryDates.map(date => {
+                return <HabitDay key={date.toString()} />
+              })}
+              {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+                return (
+                    <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+                )
+              })}
             </div>
         </div>
     )
