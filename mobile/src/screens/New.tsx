37c9a021bd80e31808efb7fad4069b578784192e
Edson Nascimento <edsnascimento89@gmail.com>
2023-02-08 11:05:00 -0300

    [new] : create interface New

---
+++ mobile/src/screens/New.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import { ScrollView, Text, TextInput, View } from "react-native";
+
+import { BackButton } from "../components/BackButton";
+import { CheckBox } from "../components/CheckBox";
+
+const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta', 'Sábado']
+
+export function New() {
+    const [ weekDays, setWeekDays ] = useState<number[]>([]);
+    
+    function handleToggleWeekDay(weekDayIndex: number){
+        if(weekDays.includes(weekDayIndex)) {
+            setWeekDays(prevState => prevState.filter(weekDay => weekDay != weekDayIndex));
+        } else {
+            setWeekDays(prevState => [...prevState, weekDayIndex]);
+        }
+    }
+
+    return (
+        <View className="flex-1 bg-background px-8 pt-16">
+            <ScrollView showsVerticalScrollIndicator={false}>
+
+                <BackButton />
+
+                <Text className="mt-6 text-white font-extrabold text-3xl">
+                    Criar hábito
+                </Text>
+
+                <Text className="mt-6 text-white font-semibold text-base">
+                    Qual seu comprometimento?
+                </Text>
+
+                <TextInput
+                    className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 border-green-600"
+                />
+
+                <Text className="font-semibold mt-4 mb-3 text-white text-base">
+                    Qual a recorrência?
+                </Text>
+
+                {
+                    availableWeekDays.map((weekDay, index) => (
+                        <CheckBox
+                            key={weekDay}
+                            title={weekDay}
+                            checked={weekDays.includes(index)}
+                            onPress={() => handleToggleWeekDay(index)}
+                        />
+                    ))
+                }
+
+
+            </ScrollView>
+        </View>
+    )
+}
