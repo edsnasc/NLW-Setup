a96db8d2a436919ffea77de579b4de11bda947ea
Edson Nascimento <edsnascimento89@gmail.com>
2023-02-16 11:04:07 -0300

    [new]: connecting api with mobile

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { ScrollView, Text, TextInput, TouchableOpacity, View, Alert } from "react-native";
 import { Feather } from '@expo/vector-icons';
+import { api } from '../lib/axios';
 
 import { BackButton } from "../components/BackButton";
 import { CheckBox } from "../components/CheckBox";
@@ -10,19 +11,43 @@ import colors from "tailwindcss/colors";
 const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta', 'Sábado']
 
 export function New() {
-    const [ weekDays, setWeekDays ] = useState<number[]>([]);
-    
-    function handleToggleWeekDay(weekDayIndex: number){
-        if(weekDays.includes(weekDayIndex)) {
+    const [title, setTitle] = useState('');
+    const [weekDays, setWeekDays] = useState<number[]>([]);
+
+    function handleToggleWeekDay(weekDayIndex: number) {
+        if (weekDays.includes(weekDayIndex)) {
             setWeekDays(prevState => prevState.filter(weekDay => weekDay != weekDayIndex));
         } else {
             setWeekDays(prevState => [...prevState, weekDayIndex]);
         }
     }
 
+    async function handleCreateNewHabit() {
+        try {
+            if (!title.trim() || weekDays.length === 0) {
+                Alert.alert('Novo Hábito', 'Informe o nome do hábito e escolha a periodicidade.')
+            }
+
+            console.log(weekDays)
+
+            await api.post('/habits', {
+                title,
+                weekDays
+            });
+
+            setTitle('');
+            setWeekDays([]);
+
+            Alert.alert('Novo Hábito', 'Hábito criado com sucesso!');
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Ops', 'Não foi possível criar o novo hábito.');
+        }
+    }
+
     return (
         <View className="flex-1 bg-background px-8 pt-16">
-            <ScrollView 
+            <ScrollView
                 showsVerticalScrollIndicator={false}
                 contentContainerStyle={{ paddingBottom: 100 }}
             >
@@ -41,6 +66,8 @@ export function New() {
                     className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
                     placeholder="Exercícios, dormir bem, etc..."
                     placeholderTextColor={colors.zinc[400]}
+                    onChangeText={setTitle}
+                    value={title}
                 />
 
                 <Text className="font-semibold mt-4 mb-3 text-white text-base">
@@ -58,11 +85,12 @@ export function New() {
                     ))
                 }
 
-                <TouchableOpacity 
+                <TouchableOpacity
                     className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
                     activeOpacity={0.7}
+                    onPress={handleCreateNewHabit}
                 >
-                    <Feather 
+                    <Feather
                         name="check"
                         size={20}
                         color={colors.white}
